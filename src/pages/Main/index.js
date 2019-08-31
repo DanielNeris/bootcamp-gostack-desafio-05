ec62ab7e401168ce4b7eecf22d4d1eaf2a865dcb
Daniel de Souza Neri <danielneris01@gmail.com>
2019-08-31 17:26:56 -0300

    default config

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Container from '../../components/Container';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>default config</h1>
+      </Container>
+    );
+  }
+}
